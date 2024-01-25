2f37ced69fdc189b838d7288230fb31bb69c91bf
Saimanasvi Charugundla <smcharu@uw.edu>
2024-01-25 11:56:17 -0800

    finished code

---
+++ README.md
@@ -0,0 +1,4 @@
+Link that contains author meta tag:
+https://www.cnn.com/politics/live-news/trump-trial-e-jean-carroll-01-25-24/index.html
+
+I changed two things from the original html code. I changed the amount of padding of the content from the outer box lines so it is more spaced out and visually appealing. I also added a light background color to differenitate the box from the rest of the page and again, look more visually appealing.
--- routes/api/v1/apiv1.js
+++ routes/api/v1/apiv1.js
@@ -10,33 +10,90 @@ router.get('/', function(req, res, next) {
 
 router.get('/urls/preview', async (req, res, next) => {
     let inputUrl = req.query.url;
-    console.log(inputUrl);
     
     try {
         let response = await fetch(inputUrl);
         let pageText = await response.text();
-        //console.log(pageText);
+        
         let htmlPage = parser.parse(pageText);
 
-        let metaTags = htmlPage.querySelectorAll("meta ");
-        //console.log("meta tags: " + metaTags);
+        let meta_title = htmlPage.querySelector('meta[property="og:title"]')?.getAttribute('content');
+        let meta_url = htmlPage.querySelector('meta[property="og:url"]')?.getAttribute('content');
+        let meta_image = htmlPage.querySelector('meta[property="og:image"]')?.getAttribute('content');
+        let meta_descrip = htmlPage.querySelector('meta[property="og:description"]')?.getAttribute('content');
+        // creative component
+        let meta_author = htmlPage.querySelector('meta[name="author"]')?.getAttribute('content');
 
-        let metaUrl = htmlPage.head.querySelector("[property~=og:url[content]").content;
-        let metaTitle = htmlPage.head.querySelector("[property~=og:title[content]").content;
-        let metaImage = htmlPage.head.querySelector("[property~=og:img").content;
-        let metaDescrip = htmlPage.head.querySelector("[property~=og:description").content;
 
-        console.log(metaUrl + "");
+        if (meta_url == null) {
+            meta_url = inputUrl;
+        }
 
-        //document.head.querySelector("[property~=video][content]").content;
+        if (meta_title == null) {
+            meta_title = htmlPage.querySelector('title')?.text;
+            if (meta_title == null) {
+                meta_title = inputUrl;
+            }
+        }
 
-        // for (let i = 0; i < metaTags.length; i++) {
-        //     let metaTag = metaTags[i];
+        if (meta_descrip == null) {
+            meta_descrip = htmlPage.querySelector('meta[name="description"]')?.getAttribute('content');;
+        }
 
-        //     if (metaTag.contains)
+        //let metaArray = [meta_title, meta_url, meta_image, meta_descrip];
 
-        // }
+        function metaTitleFunction (meta_title) {
+            if (meta_title) {
+                return `<p><strong>${meta_title}</strong></p>`;
+            } else {                
+                return "";
+            }
+        }
 
+        function metaImageFunction (meta_image) {
+            if (meta_image) {
+                return `<img src='${meta_image}' style='max-height: 200px; max-width: 270px;' ></img>`
+            } else {
+                return "";
+            }
+        }
+
+        function metaDescripFunction (meta_descrip) {
+            if (meta_descrip) {
+                return `<p>${meta_descrip}</p>`
+            } else {
+                return "";
+            }
+        }
+
+        function metaAuthorFunction (meta_author) {
+            if (meta_author) {
+                return `<p>${meta_author}</p>`
+            } else {
+                return "";
+            }
+        }
+
+        let html1 = 
+        `
+            <html>
+            <body>
+                <div style="max-width: 300px; border: solid 1px; padding: 15px; text-align: center; background-color: #f0f0f0;"> 
+                    <a href=${meta_url}>
+                        ${metaTitleFunction(meta_title)}
+                        ${metaAuthorFunction(meta_author)}
+                        ${metaImageFunction(meta_image)}
+                    </a>
+                    ${metaDescripFunction(meta_descrip)}
+                </div>
+            <body>
+            </html>
+        `
+
+        res.type("html");
+        res.send(html1);
+
+        console.log(html1);
 
     } catch (error) {
         console.log(error);
